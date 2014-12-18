56b9b3227f0cca29b89963b3e9c2e0bce62951d0
Buda Paul <polly.paul09@gmail.com>
2014-12-18 16:51:34 +0200

    string extened

---
+++ js/string.js
@@ -0,0 +1,33 @@
+
+String.prototype.capitalize = function(){
+  var new_str = this.trim();
+  return new_str.charAt(0).toUpperCase() + new_str.slice(1).toLowerCase();
+}
+
+console.log("   My name is Eric  ".capitalize());
+
+String.prototype.humanize = function(){
+  var new_str = this.replace(/[-_ ]+/g, ' ').trim()
+  return new_str.charAt(0).toUpperCase() + 
+  new_str.slice(1).toLowerCase().replace(/[-_]+/g, ' ')
+};
+
+console.log("___---lol----My_name is Eric".humanize())
+
+String.prototype.constantize=function(){
+  return this.replace(/[-_ ]*[a-zA-Z]+[-_ ]*/g,function(match){
+    var new_string = match.replace(/[-_ ]/g,'') 
+    return new_string.charAt(0).toUpperCase() + new_string.slice(1)
+  });
+
+}
+console.log("-----My name is_Eric-----".constantize())
+
+String.prototype.tableize=function(){
+
+  var a=this.replace(/[!@#$%^&*()+}{'':>?<}]+/g,'');
+  return a.replace(/[-_ ]+/g,'-');
+
+}
+
+console.log("!@#$%^My name is Eric".tableize())
